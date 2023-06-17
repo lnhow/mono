109b5dba3403eb16dc11dd5a2e66d4dc88681ffa
Hao Le <47709398+lnhow@users.noreply.github.com>
2023-06-17 09:57:31 +0700

    feat: add redux

---
+++ apps/newts-web/src/store/slices/infoSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { HYDRATE } from 'next-redux-wrapper'
+import { AppState } from '../store'
+
+export interface TInfoState {
+  now: number
+}
+
+const initialState: TInfoState = {
+  now: Date.now()
+}
+
+export const infoSlice = createSlice({
+  name: 'info',
+  initialState,
+  reducers: {
+    updateNow(state) {
+      state.now = Date.now()
+    }
+  },
+  extraReducers: {
+    // next-redux-wrapper
+    [HYDRATE]: (state, action) => {
+      return {
+        ...state,
+        ...action.payload.info,
+      }
+    },
+  },
+})
+
+export const IInfoSlice = {
+  actions: infoSlice.actions,
+  selectors: {
+    selectNow: (state: AppState) => state.info.now,
+  },
+
+}
