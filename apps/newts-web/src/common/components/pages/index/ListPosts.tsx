fb6b5723cb14e4d7fb3196d9f9d49419bad7667c
haoln <47709398+lnhow@users.noreply.github.com>
2023-09-09 16:00:26 +0700

    fix: Web reload page

--- apps/newts-web/src/common/components/pages/index/ListPosts.tsx
+++ apps/newts-web/src/common/components/pages/index/ListPosts.tsx
@@ -1,16 +1,19 @@
+import Link from 'next/link'
+
 export interface IPost {
-  id: string,
+  id: string
   attributes: {
-    slugUrl?: string,
-    title?: string,
+    slugUrl?: string
+    title?: string
     description?: string
   }
 }
 
 export const PostItem = ({ post }: { post: IPost }) => {
   return (
-    <a
+    <Link
       href={`/p/${post.attributes?.slugUrl}`}
+      title={post.attributes?.title || ''}
       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
     >
       <h2 className={'mb-3 text-2xl font-semibold'}>
@@ -22,6 +25,6 @@ export const PostItem = ({ post }: { post: IPost }) => {
       <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
         {post.attributes?.description}
       </p>
-    </a>
+    </Link>
   )
 }
