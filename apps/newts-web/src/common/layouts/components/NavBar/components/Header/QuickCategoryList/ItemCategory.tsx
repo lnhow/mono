a4da0febba6051884581f103fa1a71c4ffab1389
haoln <47709398+lnhow@users.noreply.github.com>
2023-10-13 22:04:16 +0700

    staging Add stub page

--- apps/newts-web/src/common/layouts/components/NavBar/components/Header/QuickCategoryList/ItemCategory.tsx
+++ apps/newts-web/src/common/layouts/components/NavBar/components/Header/QuickCategoryList/ItemCategory.tsx
@@ -1,15 +1,27 @@
+'use client'
 import { Button } from '@mui/material'
 import Link from 'next/link'
-import React from 'react'
+import React, { memo } from 'react'
 import { PropsItemCategory } from '../types'
+import { usePathname } from 'next/navigation'
 
-export default function ItemCategory({ data: category }: PropsItemCategory) {
+const ItemCategory = memo(function ItemCategory({ data: category }: PropsItemCategory) {
+  const pathname = usePathname()
   const linkUrl = category.attributes.slugUrl
     ? `/c/${category.attributes.slugUrl}`
     : '#'
+  const isActive = pathname === linkUrl
+  console.log('[Dev Log] -> file: ItemCategory.tsx:14 -> ItemCategory -> isActive:', isActive)
   return (
     <Button
-      className="h-10 normal-case font-light"
+      className={`
+        h-10 normal-case font-light
+        ${isActive ? 
+          'text-primary-700 dark:text-primary-300 border-b border-primary-700 dark:border-primary-300'
+          :
+          ''
+        }
+      `}
       LinkComponent={Link}
       href={linkUrl}
       color="inherit"
@@ -17,4 +29,6 @@ export default function ItemCategory({ data: category }: PropsItemCategory) {
       {category.attributes.title}
     </Button>
   )
-}
+})
+
+export default ItemCategory
