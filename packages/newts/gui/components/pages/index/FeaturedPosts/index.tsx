5f32b7d35e74fab674b01e960d79ce0ef464ab8d
haoln <47709398+lnhow@users.noreply.github.com>
2023-10-18 21:14:37 +0700

    nw-home Add stub featured layout

--- packages/newts/gui/components/pages/index/FeaturedPosts/index.tsx
+++ packages/newts/gui/components/pages/index/FeaturedPosts/index.tsx
@@ -2,8 +2,16 @@ import { Skeleton } from '@mui/material'
 
 export default async function FeaturedPosts() {
   return (
-    <div>
-      <Skeleton variant='rectangular' className='w-full h-[400px]' />
+    <div className='flex gap-4 flex-col xl:flex-row'>
+      <div className='flex flex-col gap-4'>
+        <Skeleton variant="rectangular" className='w-full xl:w-96 h-48' />  
+        <Skeleton variant="rectangular" className='w-full xl:w-96 h-48' />  
+      </div>
+      <Skeleton variant='rectangular' className='flex-4 w-full h-[400px] order-first xl:order-none' />
+      <div className='flex flex-col gap-4'>
+        <Skeleton variant="rectangular" className='w-full xl:w-96 h-48' />  
+        <Skeleton variant="rectangular" className='w-full xl:w-96 h-48' />  
+      </div>
     </div>
   )
 }
