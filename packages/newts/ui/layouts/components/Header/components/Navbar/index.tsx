a0404d877e933341c3909d0ac807fd867d477604
haoln <47709398+lnhow@users.noreply.github.com>
2023-11-29 21:18:49 +0700

    nw-filter Fix navbar divider

--- packages/newts/ui/layouts/components/Header/components/Navbar/index.tsx
+++ packages/newts/ui/layouts/components/Header/components/Navbar/index.tsx
@@ -1,25 +1,28 @@
-import { STALE_TIME } from '@newts/ui/constants/staleTime'
 import QuickCategoryList from './QuickCategoryList'
 import FullCategoryModalButton from './FullCategoryModal'
 import SearchButton from './SearchButton'
-import { NwCategoryGroupProps } from '@/types/components/category.type'
-
-export const revalidate = STALE_TIME.DEFAULT
+import { NwCategoryGroupProps } from '@newts/ui/types/components/category.type'
+import { NwVerticalDivider } from '@newts/ui/components/common/NwDivider/Vertical'
 
 export default async function Navbar({ data }: NwCategoryGroupProps) {
   return (
-    <div className='
+    <div
+      className="
       w-full dark:bg-bgprimary-800 bg-bgprimary-300
       sticky top-0 z-20
-    '>
-      <div className="
+    "
+    >
+      <nav
+        className="
         max-w-screen-2xl h-10 mx-auto px-1 sm:px-3 flex
-        divide-x divide-gray-400 dark:divide-neutral-700
-      ">
+      "
+      >
         <FullCategoryModalButton data={data} />
+        <NwVerticalDivider />
         <QuickCategoryList data={data} />
+        <NwVerticalDivider />
         <SearchButton />
-      </div>
+      </nav>
     </div>
   )
 }
