88a9e3000b6d0124baf098c156e152ffc916be90
Hao Le <47709398+lnhow@users.noreply.github.com>
2024-05-11 20:04:33 +0700

    81-feat-tools-color-picker Update color area thumb for usuabilty

--- packages/hsp/ui/modules/tools/colorPicker/components/HsColorArea.tsx
+++ packages/hsp/ui/modules/tools/colorPicker/components/HsColorArea.tsx
@@ -10,7 +10,7 @@ export default function HsColorArea({
       {...props}
       className={classNames('w-20 h-20 rounded', className)}
     >
-      <ColorThumb className="w-4 h-4 rounded-full shadow-sm border-white border" />
+      <ColorThumb className="w-4 h-4 rounded-full shadow-xl !bg-white border-slate-700 border-opacity-60 border" />
     </ColorArea>
   )
 }
