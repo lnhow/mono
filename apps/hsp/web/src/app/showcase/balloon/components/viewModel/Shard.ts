31cd7e90916cc6f3fd13fb71e842ff0148ea31f9
Hao Le <lenguyenhaoudw@gmail.com>
2024-08-26 21:25:36 +0700

    s-hpbd Add balloon showcase (#88)
    * s-hpbd Initial page
    
    * s-hpbd Drawn a firework line
    
    * s-hpbd Drawn a firework blast
    
    * s-hpbd Refactor
    
    * s-hpbd Complete animation
    
    * s-hpbd Complete gift box
    
    * s-hpbd Update page metadata

---
+++ apps/hsp/web/src/app/showcase/balloon/components/viewModel/Shard.ts
@@ -0,0 +1,63 @@
+import Color from './Color'
+import Scene from './Scene'
+import options from './options'
+
+export default class Shard {
+  x: number
+  y: number
+  dx: number
+  dy: number
+  prevPoints: number[][]
+  color: Color
+  alive: boolean
+  size: number
+
+  constructor(x: number, y: number, vx: number, vy: number, color: Color) {
+    const velocity =
+      options.firework.shard.velocity.base +
+      options.firework.shard.velocity.added * Math.random()
+
+    this.dx = vx * velocity
+    this.dy = vy * velocity
+
+    this.x = x
+    this.y = y
+
+    this.prevPoints = [[x, y]]
+    this.color = color
+
+    this.alive = true
+
+    this.size =
+      options.firework.shard.size.base +
+      options.firework.shard.size.added * Math.random()
+  }
+
+  update() {
+    const ctx = Scene.ctx
+    this.x += this.dx
+    this.y += this.dy += options.gravity // vy is affected by gravity
+
+    if (this.prevPoints.length > options.firework.shard.points) {
+      this.prevPoints.shift()
+    }
+
+    this.prevPoints.push([this.x, this.y])
+
+    const lineWidthFactor = this.size / this.prevPoints.length
+
+    for (let k = 1; k < this.prevPoints.length; k++) {
+      const point = this.prevPoints[k]
+      const prevPoint = this.prevPoints[k - 1]
+
+      ctx.strokeStyle = this.color.toAlpha(k / this.prevPoints.length)
+      ctx.lineWidth = k * lineWidthFactor
+      ctx.beginPath()
+      ctx.moveTo(point[0], point[1])
+      ctx.lineTo(prevPoint[0], prevPoint[1])
+      ctx.stroke()
+    }
+
+    if (this.prevPoints[0][1] > Scene.halfHeight) this.alive = false
+  }
+}
