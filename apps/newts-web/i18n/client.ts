4c895a71ad3dd900d0b35354395ec9b82ef2ef0f
haoln <47709398+lnhow@users.noreply.github.com>
2025-03-24 20:44:14 +0700

    v2-pnpm Remove unrelevant code

--- apps/newts-web/i18n/client.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-import initI18nextClient from '@newts/i18n/src/client'
-import { LANGUAGES, LANG_COOKIE_NAME, getOptions } from '@i18n/config'
-
-export const useTranslation = initI18nextClient({
-  getOptions,
-  languages: [...LANGUAGES],
-  langCookieName: LANG_COOKIE_NAME,
-})
