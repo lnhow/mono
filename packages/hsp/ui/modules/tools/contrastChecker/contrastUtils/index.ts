cb39f65e6f415050a13fdd4713d2292588d5602c
haoln <47709398+lnhow@users.noreply.github.com>
2025-04-24 20:41:55 +0700

    v2-eslint (fix) Typescript utils

--- packages/hsp/ui/modules/tools/contrastChecker/contrastUtils/index.ts
+++ packages/hsp/ui/modules/tools/contrastChecker/contrastUtils/index.ts
@@ -11,11 +11,12 @@ export function hexToRgb(hex = '#000000') {
   })
 
   const result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexVal)
+
   return result
     ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
+        r: parseInt(result[1] || '0', 16),
+        g: parseInt(result[2] || '0', 16),
+        b: parseInt(result[3] || '0', 16),
       }
     : null
 }
