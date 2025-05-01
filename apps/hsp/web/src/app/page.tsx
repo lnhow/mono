97018b7da9130c006fc8b6ffee784f197e4152e9
haoln <47709398+lnhow@users.noreply.github.com>
2025-05-01 13:23:27 +0700

    v2-themechanger  Add theme swicher

---
+++ apps/hsp/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ThemeToggle } from "@hsp/ui/src/components/theme/ThemeToggle";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>Welcome to HSP!</h1>
+      <p>This is the home page of your HSP application.</p>
+      <ThemeToggle />
+    </div>
+  );
+}
---
+++ packages/hsp/ui/src/components/theme/const.ts
@@ -0,0 +1,6 @@
+export enum THEME {
+  DARK = 'dark',
+  LIGHT = 'light',
+}
+
+export const DEFAULT_THEME = THEME.DARK
