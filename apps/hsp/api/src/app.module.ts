56aac090f3692bc8a916b13e1add0ec0e90d8555
haoln <47709398+lnhow@users.noreply.github.com>
2025-05-08 07:40:51 +0700

    v2-guesart Add prisma & config dependencies

--- apps/hsp/api/src/app.module.ts
+++ apps/hsp/api/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
 import { HealthCheckModule } from './modules/health/health.module'
 import { GuesartWSModule } from './modules/guesart/guesart.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [GuesartWSModule, HealthCheckModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GuesartWSModule,
+    HealthCheckModule,
+  ],
 })
 export class AppModule {}
