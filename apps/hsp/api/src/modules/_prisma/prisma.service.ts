5d0912bcc913d54e3f8c3cb9a77a5ce35d15fe5b
haoln <47709398+lnhow@users.noreply.github.com>
2025-05-10 16:17:39 +0700

    v2-guesart refactor(prisma): remove dev log from PrismaService constructor

--- apps/hsp/api/src/modules/_prisma/prisma.service.ts
+++ apps/hsp/api/src/modules/_prisma/prisma.service.ts
@@ -14,13 +14,6 @@ export class PrismaService
 {
   private logger: Logger = new Logger('PrismaService')
   constructor(configService: ConfigService) {
-    console.log(
-      '\x1B[35m[Dev log]\x1B[0m -> bootstrap -> PORT:',
-      configService.get('PORT'),
-      configService.get('API_DATABASE_URL'),
-      configService.get('API_DATABASE_NAME'),
-      configService.get('API_SOCKET_ADAPTER_COLLECTION'),
-    )
     super({
       datasources: {
         db: {
