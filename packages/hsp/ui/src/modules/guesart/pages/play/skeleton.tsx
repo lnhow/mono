f86269dbdb2fcbb185d5af1e9dad1c1993b22c5c
haoln <47709398+lnhow@users.noreply.github.com>
2025-05-22 22:30:39 +0700

    v2-guesart style(ui): adjust skeleton layout for better mobile responsiveness

--- packages/hsp/ui/src/modules/guesart/pages/play/skeleton.tsx
+++ packages/hsp/ui/src/modules/guesart/pages/play/skeleton.tsx
@@ -4,8 +4,8 @@ import SidebarSkeleton from './sidebar/skeleton'
 export default function RoomSkeleton() {
   return (
     <div className="flex flex-col md:flex-row gap-2">
-      <Skeleton className="w-full md:h-(--layout-full-height)" />
-      <SidebarSkeleton className="w-full md:w-[360px] md:max-w-4/12" />
+      <Skeleton className="w-full max-md:pb-[100%] md:h-(--layout-full-height)" />
+      <SidebarSkeleton className="w-full max-md:pb-[100%] md:w-[360px] md:max-w-4/12" />
     </div>
   )
 }
