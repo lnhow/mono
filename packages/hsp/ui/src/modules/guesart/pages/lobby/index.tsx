062687e173321f74c6d74aa926e353f84259adf3
haoln <47709398+lnhow@users.noreply.github.com>
2025-05-31 15:45:04 +0700

    docs(guesart): update metadata and page title to lowercase

--- packages/hsp/ui/src/modules/guesart/pages/lobby/index.tsx
+++ packages/hsp/ui/src/modules/guesart/pages/lobby/index.tsx
@@ -7,7 +7,7 @@ function PageLobby() {
   return (
     <div className="max-w-2xl mx-auto min-h-(--layout-full-height) flex flex-col justify-center">
       <Card className="p-6 shadow-md max-w-sm w-full mx-auto">
-        <h1 className="text-2xl text-center font-semibold mb-4">Guesart</h1>
+        <h1 className="text-2xl text-center font-semibold mb-4">guesart</h1>
         <FormGroup />
       </Card>
     </div>
