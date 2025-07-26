40f0c15605c7717b283b38cfbb1df699666b9015
Hao Le <lenguyenhaoudw@gmail.com>
2025-07-26 21:27:49 +0700

    threejs lesson 16

--- apps/learn/threejs/classic/src/16-house/script.ts
+++ apps/learn/threejs/classic/src/16-house/script.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { Timer } from 'three/addons/misc/Timer.js'
 import GUI from 'lil-gui'
+import { Sky } from 'three/addons/objects/Sky.js'
 
 /**
  * Base
@@ -17,18 +18,18 @@ const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 
 // Floor ==============================
-const floorAlphaTexture = textureLoader.load('/16-house/ground/alpha.jpg')
+const floorAlphaTexture = textureLoader.load('/16-house/ground/alpha.webp')
 const floorColorTexture = textureLoader.load(
-  '/16-house/ground/brown_mud_leaves_01_diff_1k.jpg',
+  '/16-house/ground/brown_mud_leaves_01_diff_1k.webp',
 )
 const floorARMTexture = textureLoader.load(
-  '/16-house/ground/brown_mud_leaves_01_arm_1k.jpg',
+  '/16-house/ground/brown_mud_leaves_01_arm_1k.webp',
 )
 const floorNormalTexture = textureLoader.load(
-  '/16-house/ground/brown_mud_leaves_01_nor_gl_1k.jpg',
+  '/16-house/ground/brown_mud_leaves_01_nor_gl_1k.webp',
 )
 const floorDisplacementTexture = textureLoader.load(
-  '/16-house/ground/brown_mud_leaves_01_disp_1k.jpg',
+  '/16-house/ground/brown_mud_leaves_01_disp_1k.webp',
 )
 
 floorColorTexture.colorSpace = THREE.SRGBColorSpace
@@ -55,7 +56,7 @@ const floor = new THREE.Mesh(
     normalMap: floorNormalTexture,
     displacementMap: floorDisplacementTexture,
     displacementScale: 0.34,
-    displacementBias: -0.15,
+    displacementBias: 0.025,
   }),
 )
 gui
@@ -80,14 +81,14 @@ const houseWallHeight = 2.5
 const houseWallSize = 4
 
 const houseWallsTexture = textureLoader.load(
-  '/16-house/walls/castle_brick_broken_06_diff_1k.jpg',
+  '/16-house/walls/castle_brick_broken_06_diff_1k.webp',
 )
 houseWallsTexture.colorSpace = THREE.SRGBColorSpace
 const houseWallsARMTexture = textureLoader.load(
-  '/16-house/walls/castle_brick_broken_06_arm_1k.jpg',
+  '/16-house/walls/castle_brick_broken_06_arm_1k.webp',
 )
 const houseWallsNormalTexture = textureLoader.load(
-  '/16-house/walls/castle_brick_broken_06_nor_gl_1k.jpg',
+  '/16-house/walls/castle_brick_broken_06_nor_gl_1k.webp',
 )
 
 const houseWalls = new THREE.Mesh(
@@ -106,14 +107,14 @@ house.add(houseWalls)
 const houseRoofHeight = 2
 
 const houseRoofTexture = textureLoader.load(
-  '/16-house/roof/wood_peeling_paint_weathered_diff_1k.jpg',
+  '/16-house/roof/wood_peeling_paint_weathered_diff_1k.webp',
 )
 houseRoofTexture.colorSpace = THREE.SRGBColorSpace
 const houseRoofARMTexture = textureLoader.load(
-  '/16-house/roof/wood_peeling_paint_weathered_arm_1k.jpg',
+  '/16-house/roof/wood_peeling_paint_weathered_arm_1k.webp',
 )
 const houseRoofNormalTexture = textureLoader.load(
-  '/16-house/roof/wood_peeling_paint_weathered_nor_gl_1k.jpg',
+  '/16-house/roof/wood_peeling_paint_weathered_nor_gl_1k.webp',
 )
 ;[houseRoofTexture, houseRoofARMTexture, houseRoofNormalTexture].forEach((texture: THREE.Texture) => {
   texture.wrapS = THREE.RepeatWrapping
@@ -134,10 +135,29 @@ houseRoof.position.y = houseWallHeight + houseRoofHeight / 2 // Offset the roof
 houseRoof.rotation.y = Math.PI * 0.25
 house.add(houseRoof)
 
+const houseDoorTexture = textureLoader.load('/16-house/door/color.webp')
+const houseDoorAlphaTexture = textureLoader.load('/16-house/door/alpha.webp')
+const houseDoorAOTexture = textureLoader.load('/16-house/door/ambientOcclusion.webp')
+const houseDoorHeightTexture = textureLoader.load('/16-house/door/height.webp')
+const houseDoorNormalTexture = textureLoader.load('/16-house/door/normal.webp')
+const houseDoorMetalnessTexture = textureLoader.load('/16-house/door/metalness.webp')
+const houseDoorRoughnessTexture = textureLoader.load('/16-house/door/roughness.webp')
+
 const houseDoorHeight = 2.2
 const houseDoor = new THREE.Mesh(
-  new THREE.PlaneGeometry(2.2, houseDoorHeight),
-  new THREE.MeshStandardMaterial({ color: 'red' }),
+  new THREE.PlaneGeometry(2.2, houseDoorHeight, 100, 100),
+  new THREE.MeshStandardMaterial({ 
+    map: houseDoorTexture,
+    transparent: true,
+    alphaMap: houseDoorAlphaTexture,
+    aoMap: houseDoorAOTexture,
+    metalnessMap: houseDoorMetalnessTexture,
+    roughnessMap: houseDoorRoughnessTexture,
+    displacementMap: houseDoorHeightTexture,
+    displacementScale: 0.15,
+    displacementBias: -0.04,
+    normalMap: houseDoorNormalTexture,
+  }),
 )
 houseDoor.position.set(
   0,
@@ -146,29 +166,66 @@ houseDoor.position.set(
 )
 house.add(houseDoor)
 
+houseWalls.castShadow = true
+houseWalls.receiveShadow = true
+houseRoof.castShadow = true 
+floor.receiveShadow = true
+
 scene.add(house)
 // End of house =======================
 
 // Bushes =============================
+const bushTexture = textureLoader.load('/16-house/bushes/forest_leaves_03_diff_1k.webp')
+bushTexture.colorSpace = THREE.SRGBColorSpace
+const bushARMTexture = textureLoader.load('/16-house/bushes/forest_leaves_03_arm_1k.webp')
+const bushNormalTexture = textureLoader.load('/16-house/bushes/forest_leaves_03_nor_gl_1k.webp')
+
 const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
-const bushMaterial = new THREE.MeshStandardMaterial()
+const bushMaterial = new THREE.MeshStandardMaterial({
+  color: 0xccffee,
+  map: bushTexture,
+  aoMap: bushARMTexture,
+  metalnessMap: bushARMTexture,
+  roughnessMap: bushARMTexture,
+  normalMap: bushNormalTexture,
+})
 
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush1.scale.set(0.5, 0.5, 0.5)
 bush1.position.set(2, 0.25, 2)
-scene.add(bush1)
+bush1.rotation.x = -0.75
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.5, 1, 0.5)
+bush2.position.set(-2.2, 0.25, 2.2)
+bush2.rotation.z = -0.5
+bush2.rotation.x = -0.3
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.8, 0.7, 1)
+bush3.position.set(2.5, 0.25, -2.3)
+bush3.rotation.x = 2
+bush3.rotation.z = -1.4
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.3, 0.4, 0.3)
+bush4.position.set(2.6, 0.25, 1.6)
+bush4.rotation.x = 2
+bush4.rotation.z = -1.4
+
+scene.add(bush1, bush2, bush3, bush4)
 // End of Bushes ======================
 
 // Rocks ==============================
 const rockTexture = textureLoader.load(
-  '/16-house/rocks/plastered_stone_wall_diff_1k.jpg',
+  '/16-house/rocks/plastered_stone_wall_diff_1k.webp',
 )
 rockTexture.colorSpace = THREE.SRGBColorSpace
 const rockARMTexture = textureLoader.load(
-  '/16-house/rocks/plastered_stone_wall_arm_1k.jpg',
+  '/16-house/rocks/plastered_stone_wall_arm_1k.webp',
 )
 const rockNormalTexture = textureLoader.load(
-  '/16-house/rocks/plastered_stone_wall_nor_gl_1k.jpg',
+  '/16-house/rocks/plastered_stone_wall_nor_gl_1k.webp',
 )
 
 const rockGeometry = new THREE.DodecahedronGeometry(0.5)
@@ -205,6 +262,9 @@ for (let i = 0; i < 30; i++) {
     (Math.random() - 0.5) * 0.5,
   )
   rock.scale.set(Math.random() + 0.5, Math.random() + 0.5, Math.random() + 0.5)
+
+  rock.castShadow = true
+  rock.receiveShadow = true
   rocks.add(rock)
 }
 scene.add(rocks)
@@ -214,13 +274,58 @@ scene.add(rocks)
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#008BFF', 0.7)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+const directionalLight = new THREE.DirectionalLight('#008BFF', 3)
 directionalLight.position.set(3, 2, -8)
 scene.add(directionalLight)
+directionalLight.castShadow = true
+
+// Optimization
+directionalLight.shadow.mapSize.width = 256
+directionalLight.shadow.mapSize.height = 256
+directionalLight.shadow.camera.top = 8
+directionalLight.shadow.camera.right = 8
+directionalLight.shadow.camera.bottom = - 8
+directionalLight.shadow.camera.left = - 8
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 20
+
+const doorLight = new THREE.PointLight('#ff7d46', 5)
+doorLight.position.set(0, 2.2, 2.5)
+house.add(doorLight)
+
+/**
+ * Fireflies
+ */
+const firefly1 = new THREE.PointLight('#D88E26', 6, 0.2, 0.4)
+const firefly2 = new THREE.PointLight('#F98726', 3, 0.4, 0.2)
+const firefly3 = new THREE.PointLight('#ED4926', 6, 0.5, 0.7)
+firefly1.castShadow = true
+firefly2.castShadow = true
+firefly3.castShadow = true
+
+scene.add(firefly1, firefly2, firefly3)
+
+/**
+ * Sky
+ */
+const sky = new Sky()
+sky.scale.set(100, 100, 100)
+scene.add(sky)
+sky.material.uniforms['turbidity'].value = 10
+sky.material.uniforms['rayleigh'].value = 3
+sky.material.uniforms['mieCoefficient'].value = 0.1
+sky.material.uniforms['mieDirectionalG'].value = 0.95
+sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95)
+
+/**
+ * Fog
+ */
+// scene.fog = new THREE.Fog('#19243A', 1, 13) // Color, start, end - Good for controlling precise values
+scene.fog = new THREE.FogExp2('#19243A', 0.1) // Color, density - Calculate the "fog strength" dynamically
 
 /**
  * Sizes
@@ -269,6 +374,8 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 })
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -281,6 +388,31 @@ const tick = () => {
   // Timer
   timer.update()
   const elapsedTime = timer.getElapsed()
+
+  // Fireflies
+  const firefly1Angle = elapsedTime * 0.3
+  firefly1.position.set(
+    Math.sin(firefly1Angle) * 1 + 4,
+    Math.max(Math.sin(firefly1Angle) * Math.sin(firefly1Angle * 2.34) * Math.sin(firefly1Angle * 3.45) * 0.3, 0.16),
+    Math.cos(firefly1Angle) * 1 + 4,
+  )
+  firefly1.intensity = Math.max(Math.sin(elapsedTime * 48) * Math.sin(firefly1Angle * 5.34) * 20, 8)
+  
+  const firefly2Angle = -elapsedTime * 0.4
+  firefly2.position.set(
+    Math.sin(firefly2Angle) * 2 + -3.5,
+    Math.max(Math.sin(firefly2Angle) * Math.sin(firefly2Angle * 3.34) * Math.sin(firefly2Angle * 2.1) * 0.2, 0.14),
+    Math.cos(firefly2Angle) * 3 + 3.5,
+  )
+  firefly2.intensity = Math.max(Math.sin(elapsedTime * 27) * Math.sin(firefly2Angle * 4.34) * 6, 5)
+  
+  const firefly3Angle = elapsedTime * 0.223
+  firefly3.position.set(
+    Math.sin(firefly3Angle) * 4 + 3,
+    Math.max(Math.sin(firefly3Angle) * Math.sin(firefly3Angle * 3.34) * Math.sin(firefly3Angle * 2.1) * 0.3, 0.1),
+    Math.cos(firefly3Angle) * 2.5 + -4,
+  )
+  firefly2.intensity = Math.max(Math.sin(elapsedTime * 18) * Math.sin(firefly3Angle * 1.34) * 10, 3)
 
   // Update controls
   controls.update()
