541bd6b68fb7cc6083878a2358f7f132a21d8c16
Hao Le <lenguyenhaoudw@gmail.com>
2025-08-11 22:49:05 +0700

    fix: remove deployment configuration for hsp-api application

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,17 +12,4 @@ module.exports = {
       log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
     }
   ],
-  deploy: {
-    hsp_api_production: {
-      user: process.env.MN_PM2_USER,
-      host: process.env.MN_PM2_HOST,
-      ref: "origin/deploy",
-      repo: "git@github.com:lnhow/mono.git",
-      path: `${process.env.MN_PM2_PATH}/hsp-api`,
-      "pre-setup": "pwd",
-      "post-setup": "ls -la && npm install --global corepack@latest turbo@2.5.5 && corepack enable pnpm",
-      "post-deploy": "apps/hsp/api/.ci/post-deploy.sh",
-      ssh_options: "ForwardAgent=yes"
-    }
-  }
 }
