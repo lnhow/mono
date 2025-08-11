4fc311cd66a9436349068551ff6f55688aa20f56
Hao Le <lenguyenhaoudw@gmail.com>
2025-08-11 22:27:34 +0700

    fix: add logging configuration for hsp-api application

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,10 @@ module.exports = {
       args: ["deploy:start", "--filter", "@hsp/app-api"],
       instances: 1,
       autorestart: true,
+      error_file: '~/logs/hsp-api/err.log',
+      out_file: '~/logs/hsp-api/out.log',
+      log_file: '~/logs/hsp-api/combined.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
     }
   ],
   deploy: {
