d7beb2187c6f0beb26832c471c6c0839e2d9770f
Hao Le <lenguyenhaoudw@gmail.com>
2025-08-12 20:24:19 +0700

    fix: refactor build and start scripts for hsp-api and add ecosystem configuration

--- apps/hsp/api/.ci/start.sh
+++ apps/hsp/api/.ci/start.sh
@@ -1,1 +1,1 @@
-pm2 start --name hsp-api pnpm -- deploy:start --filter=@hsp/app-api
+pm2 startOrReload ecosystem.config.js --only hsp-api
---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    { 
+      name: "hsp-api",
+      script: "pnpm",
+      args: ["-C", "out", "deploy:start", "--filter", "@hsp/app-api"],
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      error_file: '~/logs/hsp-api/err.log',
+      out_file: '~/logs/hsp-api/out.log',
+      log_file: '~/logs/hsp-api/combined.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
+    }
+  ],
+}
