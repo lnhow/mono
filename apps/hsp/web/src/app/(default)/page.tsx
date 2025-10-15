42fb674e4c9da4024ac9e622fc53e18509f755c2
Hao Le <lenguyenhaoudw@gmail.com>
2025-10-15 21:05:43 +0700

    feat: Add latest posts section to HomePage and enhance post visibility logic

--- apps/hsp/web/src/app/(default)/page.tsx
+++ apps/hsp/web/src/app/(default)/page.tsx
@@ -1,7 +1,21 @@
 import CardsDemo from '@hsp/ui/src/modules/default/modules/home/page'
+import CardPost from '@hsp/ui/src/modules/posts/card'
+import { PostUtils } from '@hsp/ui/src/modules/posts/utils'
+import { allPosts } from 'content-collections'
 import { Metadata } from 'next'
 
 export default function HomePage() {
+  // Get the latest 10 posts
+  const posts = allPosts
+    .filter((post) => PostUtils.shouldShow(post.draft) && post.slug !== 'test')
+    .sort((a, b) => {
+      // Sort by date descending
+      const dateA = new Date(a.updatedAt || a.createdAt).getTime()
+      const dateB = new Date(b.updatedAt || b.createdAt).getTime()
+      return dateB - dateA
+    })
+    .slice(0, 9)
+
   return (
     <div className="flex flex-col gap-4 max-w-7xl mx-auto md:px-4 py-8">
       <section className="mt-[18%]">
@@ -13,6 +27,23 @@ export default function HomePage() {
         <h2 className="text-md text-fore-200">
           Web Developer. Photography and UX Enthusiast.
         </h2>
+      </section>
+      <section className="mt-8">
+        <h2 className="text-2xl mb-2">Latest Posts</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {posts.map((post) => (
+            <CardPost
+              key={post.slug}
+              title={post.title}
+              description={post.description}
+              readingTimeMinutes={post.readingTime}
+              createdAt={post.createdAt}
+              updatedAt={post.updatedAt}
+              slug={post.slug}
+              tags={post.tags || []}
+            />
+          ))}
+        </div>
       </section>
       <section className="mt-8">
         <h2 className="text-2xl mb-2">Things I built for fun</h2>
--- apps/hsp/web/src/app/(default)/posts/[slug]/page.tsx
+++ apps/hsp/web/src/app/(default)/posts/[slug]/page.tsx
@@ -4,9 +4,10 @@ import { notFound } from 'next/navigation'
 import { MDXContent } from '@content-collections/mdx/react'
 
 import { mdxComponents } from '@/mdx-components'
-import { isProductionEnv } from '@/common/utils/common'
 import MarkdownTypography from '@hsp/ui/src/components/mdx/typography'
 import TableOfContents from '@hsp/ui/src/components/mdx/toc'
+import { PostUtils } from '@hsp/ui/src/modules/posts/utils'
+import ViewTransition from '@hsp/ui/src/components/app/ViewTransition'
 
 interface PostPageProps {
   params: Promise<{
@@ -16,7 +17,7 @@ interface PostPageProps {
 
 const getPost = (slug: string) => {
   return allPosts.find((post) => {
-    if (isProductionEnv && post.draft) {
+    if (!PostUtils.shouldShow(post.draft)) {
       return false
     }
     return post.slug === slug
@@ -27,7 +28,7 @@ const getPost = (slug: string) => {
 export function generateStaticParams() {
   return allPosts.map((post) => ({
     slug: post.slug,
-  }));
+  }))
 }
 
 export async function generateMetadata({
@@ -81,50 +82,59 @@ export default async function PostPage({ params }: PostPageProps) {
     day: 'numeric',
   })
   const toc = post.toc ? JSON.parse(post.toc) : []
+  const transitionName = PostUtils.getTransitionName(slug)
 
   return (
     // Recommended max width for comfortable reading is 65-75 characters
     <div className="mx-auto my-8 [--w-content:75ch]">
       <header className="flex gap-8 text-fore-200">
-        <div className="border-b border-fore-100 flex-1 max-w-full pb-6">
-          <div className="mx-auto w-(--w-content) max-w-full">
-            <div className="font-mono text-sm">
-              <time dateTime={post.createdAt.toISOString()}>{displayDate}</time>
-              {post.readingTime && (
-                <>
-                  <span> • </span>
-                  <span>{Math.ceil(post.readingTime)} min read</span>
-                </>
-              )}
-            </div>
-            {post.updatedAt && (
-              <div className="font-mono text-xs mt-2">
-                (Updated:{' '}
-                <time dateTime={post.updatedAt.toISOString()}>
-                  {post.updatedAt.toLocaleDateString('en-US', {
-                    year: 'numeric',
-                    month: 'short',
-                    day: 'numeric',
-                  })}
+        <ViewTransition name={transitionName.card}>
+          <div className="flex-1 max-w-full pb-6">
+            <div className="mx-auto w-(--w-content) max-w-full">
+              <div className="font-mono text-sm">
+                <time dateTime={post.createdAt.toISOString()}>
+                  {displayDate}
                 </time>
-                )
+                {post.readingTime && (
+                  <>
+                    <span> • </span>
+                    <span>{Math.ceil(post.readingTime)} min read</span>
+                  </>
+                )}
               </div>
-            )}
-            <h1 className="text-3xl md:text-4xl font-semibold mt-6 mb-3 text-fore-400">
-              {post.title}
-            </h1>
-            <div>
-              {post.tags?.length && (
-                <div className="mt-2 flex flex-wrap gap-2 text-xs font-mono">
-                  {post.tags?.map((tag) => (
-                    <span key={tag}>#{tag.toLowerCase()}</span>
-                  ))}
+              {post.updatedAt && (
+                <div className="font-mono text-xs mt-2">
+                  (Updated:{' '}
+                  <time dateTime={post.updatedAt.toISOString()}>
+                    {post.updatedAt.toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: 'short',
+                      day: 'numeric',
+                    })}
+                  </time>
+                  )
                 </div>
               )}
-              <p className="text-md break-words mt-4">{post.description}</p>
+              <ViewTransition name={transitionName.title}>
+                <h1 className="text-3xl md:text-4xl font-semibold mt-6 mb-3 text-fore-400">
+                  {post.title}
+                </h1>
+              </ViewTransition>
+              <div>
+                {post.tags?.length && (
+                  <div className="mt-2 flex flex-wrap gap-2 text-xs font-mono">
+                    {post.tags?.map((tag) => (
+                      <span key={tag}>#{tag.toLowerCase()}</span>
+                    ))}
+                  </div>
+                )}
+                <ViewTransition name={transitionName.description}>
+                  <p className="text-md break-words mt-4">{post.description}</p>
+                </ViewTransition>
+              </div>
             </div>
           </div>
-        </div>
+        </ViewTransition>
         <div className="basis-xs hidden lg:block min-w-60">&nbsp;</div>
       </header>
       <div className="flex gap-8 mt-8 relative text-fore-200">
