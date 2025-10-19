45cb7bc2ac84d694730254e8ff33a280c5e99303
Hao Le <lenguyenhaoudw@gmail.com>
2025-10-19 13:35:26 +0700

    refactor: Remove unused layout and page components for tools and locale

--- apps/hsp/web/src/app/_[locale]/(default)/tools/(tools)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { BaseLayoutProps } from '@hsp/ui/layouts/types'
-import { default as BaseToolsLayout } from '@hsp/ui/modules/tools/layout'
-
-export default function ToolsLayout({ children }: BaseLayoutProps) {
-  return <BaseToolsLayout>{children}</BaseToolsLayout>
-}
