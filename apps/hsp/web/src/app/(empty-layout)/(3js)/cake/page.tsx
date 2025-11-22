d31d8805cd26cffe5938eabbf25f049265963a09
Hao Le <lenguyenhaoudw@gmail.com>
2025-11-22 22:46:09 +0700

    feat: Wrap Page component in Suspense for improved loading handling

--- apps/hsp/web/src/app/(empty-layout)/(3js)/cake/page.tsx
+++ apps/hsp/web/src/app/(empty-layout)/(3js)/cake/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next'
 import Main from './_experience'
 import { Provider } from 'jotai'
-import { ViewTransition } from 'react'
+import { Suspense, ViewTransition } from 'react'
 
 export const metadata: Metadata = {
   title: 'Cake',
@@ -9,21 +9,23 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Cake',
     description: 'Someone sent a cake!',
-  }
+  },
 }
 
 export default function Page() {
   return (
-    <ViewTransition name="cake-card" update="none">
-      <main className="fixed top-0 left-0 w-full h-full">
-        <Provider>
-          <Main />
-        </Provider>
-        <div className="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-neutral-900">
-          <h1 className='sr-only'>Cake</h1>
-          <p>Loading...</p>
-        </div>
-      </main>
-    </ViewTransition>
+    <Suspense>
+      <ViewTransition name="cake-card" update="none">
+        <main className="fixed top-0 left-0 w-full h-full">
+          <Provider>
+            <Main />
+          </Provider>
+          <div className="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-neutral-900">
+            <h1 className="sr-only">Cake</h1>
+            <p>Loading...</p>
+          </div>
+        </main>
+      </ViewTransition>
+    </Suspense>
   )
 }
